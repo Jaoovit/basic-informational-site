9818b3ef29435d2a7465e1255ce231c0df84b02e
João Oliveira <jaoovit@gmail.com>
2024-08-12 13:50:53 +0100

    implement express framework

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
+/*
 import http from "http";
 import fs from "fs/promises";
 import url from "url";
 import path from "path";
+*/
+
 import "dotenv/config";
+import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
 
-const PORT = process.env.PORT;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-const __filename = url.fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-console.log(path.join(__dirname, "src", "index.html"));
+app.use(express.static(path.join(__dirname, "src")));
 
+/*
 const server = http.createServer(async (req, res) => {
   let filePath;
   if (req.url === "/") {
@@ -28,7 +36,14 @@ const server = http.createServer(async (req, res) => {
   res.write(data);
   res.end();
 });
+*/
 
+app.listen(PORT, () =>
+  console.log(`This server is running in the ${PORT} port`)
+);
+
+/*
 server.listen(PORT, () => {
   console.log(`This server is running in the ${PORT} port`);
 });
+*/
